7f8c7c90db1bf7b7ae75e370acbb11d2a2531502
livaoc <livlexia247@gmail.com>
2025-07-30 22:34:18 -0300

    Finished exercise 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split("").reverse().join("");
+    }
+    arr = arr.reverse();
+    return string = arr.join(" ");
 };
 
 // Do not edit below this line
