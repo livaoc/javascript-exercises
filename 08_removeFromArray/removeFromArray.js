1a816f58c62d4802909fb9321d8ea9e9d2230847
livaoc <livlexia247@gmail.com>
2025-08-01 13:26:55 -0300

    Finished exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...args) {
+    for(let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
