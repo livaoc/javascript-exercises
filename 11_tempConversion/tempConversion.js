713c14f388e56c447ee5676e5cd6e5ce26995f38
livaoc <livlexia247@gmail.com>
2025-08-01 14:16:04 -0300

    Finished exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedToC = (fahrenheit - 32) * (5/9);
+  if (!Number.isInteger(convertedToC)) {
+    convertedToC = Math.round(convertedToC * 10) / 10;
+    return convertedToC;
+  } else {
+    return convertedToC;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedToF = (celsius * 9/5) + 32;
+  if (!Number.isInteger(convertedToF)) {
+    convertedToF = Math.round(convertedToF * 10) / 10;
+    return convertedToF;
+  } else {
+    return convertedToF;
+  }
 };
 
 // Do not edit below this line
